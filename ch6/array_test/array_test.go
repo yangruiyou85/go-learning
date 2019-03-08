e3385d93f9e10317b74fd4eadf6d2da2361fe6e4
yangruiyou <yangruiyou@wiz.top>
2019-03-08 08:45:36 +0800

    'algo'

--- ch6/array_test/array_test.go
+++ ch6/array_test/array_test.go
@@ -1,14 +1,16 @@
+
+
+
 package arrary_test
 
-import (
-	"testing"
-)
+import "testing"
 
-func TestArrayInit(t *testing.T) {
-	var arr [3]int
-	arr1 := [4]int{1, 2, 3, 4}
-	arr3 := [...]int{1, 3, 4, 5}
-	t.Log(arr[1], arr1[2])
 
-	t.Log(arr1, arr3, arr)
+func TestArrayInit(t *testing.T){
+  var arr[3]int
+  arr1:=[4]int{1,2,3,4}
+  arr3:=[...]int{1,3,4,5}
+  t.Log(arr[1],arr[2])
+
+  t.Log(arr1,arr3)
 }
