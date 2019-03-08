76a88d233f33901012164f5adf92febef4f288a9
ruiyou.yang <ruiyou.yang@gmail.com>
2019-03-08 21:35:55 +0800

    ch6

--- ch6/array_test/array_test.go
+++ ch6/array_test/array_test.go
@@ -8,7 +8,23 @@ func TestArrayInit(t *testing.T) {
 	var arr [3]int
 	arr1 := [4]int{1, 2, 3, 4}
 	arr3 := [...]int{1, 3, 4, 5}
-	t.Log(arr[1], arr1[2])
+	t.Log(arr[1], arr[2])
 
 	t.Log(arr1, arr3, arr)
 }
+
+func TestArrTravel(t *testing.T) {
+	arr3 := [...]int{1, 3, 4, 5}
+	for idx, v := range arr3 {
+		t.Log(idx, v)
+	}
+
+}
+
+func TestArraySection(t *testing.T) {
+	arr3 := [...]int{1, 2, 3, 4, 5}
+	arr3_sec := arr3[3:]
+	t.Log(arr3_sec)
+}
+
+//slice
---
+++ ch6/slice_test/slice_test.go
@@ -0,0 +1,40 @@
+package slice_test
+
+import "testing"
+
+func TestSliceInit(t *testing.T) {
+	var s0 []int
+	t.Log(s0, len(s0), cap(s0))
+
+	s0 = append(s0, 1)
+	t.Log(len(s0), cap(s0))
+
+	s1 := []int{1, 2, 3, 4}
+	t.Log(len(s1), cap(s1))
+
+	s2 := make([]int, 3, 5)
+	t.Log(len(s2), cap(s2))
+
+	t.Log(s2[0], s2[1], s2[2])
+	t.Log(len(s2), cap(s2))
+
+	s2 = append(s2, 4)
+	t.Log(s2[0], s2[1], s2[2], s2[3])
+	t.Log(len(s2), cap(s2))
+
+}
+
+func TestSliceGrowing(t *testing.T) {
+	s := []int{}
+	for i := 0; i < 10; i++ {
+		s = append(s, i)
+		t.Log(len(s), cap(s))
+
+	}
+}
+
+func TestSliceShareMemory(t *testing.T) {
+	year := []string{"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"}
+	Q2 := year[3:6]
+	t.Log(Q2, len(Q2), cap(Q2))
+}
